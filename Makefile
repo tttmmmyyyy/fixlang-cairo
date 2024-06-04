4fa594b20a4cc10587d67d6fc87512db7f499cbd
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2024-06-04 21:53:54 +0900

    improve xlib binding

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@ clean:
 	rm -f libcairo_backend_x11.so
 
 example_image:
-	fix build -o i.out -f ./examples/hello_world_image.fix cairo.fix cairo.image_surface.fix -d cairo
+	fix build -f ./examples/hello_world_image.fix cairo.fix cairo.image_surface.fix -d cairo
 
 example_xlib: libcairo_fixlang_xlib_surface.so
-	fix build -o x.out -f ./examples/hello_world_xlib.fix cairo.fix cairo.xlib_surface.fix -L. -d cairo_fixlang_xlib_surface cairo X11 Xext
+	fix build -f ./examples/hello_world_xlib.fix cairo.fix cairo.xlib_surface.fix -L. -d cairo_fixlang_xlib_surface cairo X11 Xext
 
 example_game_of_life_xlib: libcairo_fixlang_xlib_surface.so
-	fix build -o l.out -f ./examples/game_of_life_xlib.fix cairo.fix cairo.xlib_surface.fix -L. -d cairo_fixlang_xlib_surface cairo X11 Xext
+	fix build -f ./examples/game_of_life_xlib.fix cairo.fix cairo.xlib_surface.fix -L. -d cairo_fixlang_xlib_surface cairo X11 Xext
 
 libcairo_fixlang_xlib_surface.so: cairo.xlib_surface.c
 	gcc $(CAIRO_FLAGS) $(X11_FLAGS) -shared -fPIC -o libcairo_fixlang_xlib_surface.so cairo.xlib_surface.c
--- cairo.xlib_surface.c
+++ cairo.xlib_surface.c
@@ -2,6 +2,7 @@
 #include <cairo/cairo-xlib.h>
 #include <X11/extensions/Xdbe.h>
 #include <cairo.h>
+#include <stdlib.h>
 
 Window cairo_fixlang_xlib_surface_create_x_window(Display *display, int32_t width, int32_t height)
 {
@@ -11,17 +12,37 @@ Window cairo_fixlang_xlib_surface_create_x_window(Display *display, int32_t widt
                                      WhitePixel(display, DefaultScreen(display)),
                                      BlackPixel(display, DefaultScreen(display)));
     XMapWindow(display, win);
-    XSelectInput(display, win, ExposureMask);
-    while (1)
-    {
-        XNextEvent(display, &event);
-        if (event.type == Expose)
-            break;
-    }
     return win;
 }
 
 cairo_surface_t *cairo_fixlang_xlib_surface_create_x_surface(Display *display, Window win, int32_t width, int32_t height)
 {
     return cairo_xlib_surface_create(display, win, DefaultVisual(display, 0), width, height);
+}
+
+XEvent *cairo_fixlang_xlib_allocate_event()
+{
+    return (XEvent *)malloc(sizeof(XEvent));
+}
+
+void cairo_fixlang_xlib_free_event(XEvent *event)
+{
+    free(event);
+}
+
+XEvent *cairo_fixlang_xlib_next_event(Display *display)
+{
+    XEvent *event = cairo_fixlang_xlib_allocate_event();
+    XNextEvent(display, event);
+    return event;
+}
+
+int32_t cairo_fixlang_xlib_event_type(XEvent *event)
+{
+    return event->type;
+}
+
+void cairo_fixlang_xlib_as_expose_event(XEvent *event, XExposeEvent *expose_event)
+{
+    *expose_event = event->xexpose;
 }
